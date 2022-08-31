a9b6649c0cbba500dcf8274584cf12695802ee18
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-09-01 00:41:30 +0530

    Set animations for one page

---
+++ lib/onboarding.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    final pages = [
+
+
+      Container(
+        color: Color(0xfff46d37),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Image.asset('assets/marketing3.png'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Web App Development',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to mak',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  ButtonTheme(
+                    height: 100,
+                    minWidth: 250,
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        side: BorderSide(
+                          color: Colors.white,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      child: Text(
+                        'get Start',
+                        style: TextStyle(fontSize: 20,
+                        color: Colors.white),
+
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ];
+    return Scaffold(
+      body: LiquidSwipe(
+        pages: pages,
+        slideIconWidget: Icon(Icons.arrow_back_ios),
+        // onPageChangeCallback: pageChangeCallback,
+        waveType: WaveType.liquidReveal,
+        // liquidController: liquidController,
+        enableSideReveal: true,
+        ignoreUserGestureWhileAnimating: true,
+
+        )
+    );
+  }
+}
