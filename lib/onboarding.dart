9784baf1f847f6e51f3258e8a1f7f0c67658d4dd
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-09-01 00:47:53 +0530

    Set animations for Three pages and recorded video

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -13,7 +13,119 @@ class _OnBoardingState extends State<OnBoarding> {
   Widget build(BuildContext context) {
     final pages = [
 
+      Container(
+        color: Color(0xff3937bf),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Image.asset('assets/marketing1.jfif'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Best IT solutions',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to mak',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  ButtonTheme(
+                    height: 100,
+                    minWidth: 250,
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        side: BorderSide(
+                          color: Colors.white,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      child: Text(
+                        'Skip',
+                        style: TextStyle(fontSize: 20,
+                            color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
 
+      Container(
+        color: Color(0xff27b57f),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Image.asset('assets/marketing2.png'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Mobile App Development',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to mak',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  ButtonTheme(
+                    height: 100,
+                    minWidth: 250,
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        side: BorderSide(
+                          color: Colors.white,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      child: Text(
+                        'Skip',
+                        style: TextStyle(fontSize: 20,
+                            color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
       Container(
         color: Color(0xfff46d37),
         child: Column(
